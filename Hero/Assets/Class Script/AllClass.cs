1c714436d2636c028e8fbf9624dbcf76806b5a9a
unknown <vasax.levy@gmail.com>
2016-12-26 06:49:53 +0100

    creation des sprites plus mise en place des animations

---
+++ Hero/Assets/Class Script/AllClass.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Test
+{
+    abstract class AllClass
+    {
+
+        private int pointDeVie;
+        private int speed;
+
+        public int PointDeVie
+        {
+            get
+            {
+                return pointDeVie;
+            }
+
+            set
+            {
+                pointDeVie = value;
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+
+            set
+            {
+                speed = value;
+            }
+        }
+    }
+}
