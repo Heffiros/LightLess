3659698e89224f2f344f1910bdde0a13d81adcd8
unknown <vasax.levy@gmail.com>
2016-12-28 00:40:58 +0100

    fin des animation plus saut

--- Hero/Assets/Hero/Hero Script/Walk.cs
+++ Hero/Assets/Hero/Hero Script/Walk.cs
@@ -4,10 +4,16 @@ using System.Collections;
 public class Walk : MonoBehaviour {
 
     Animator anim;
+    private Rigidbody2D heroBody;
+
     public float speedMoove;
+    private bool isJumping;
+
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
+        heroBody = GetComponent<Rigidbody2D>();
+        isJumping = false;
 	}
 	
 	// Update is called once per frame
@@ -18,7 +24,14 @@ public class Walk : MonoBehaviour {
             float move = Input.GetAxis("Horizontal");
             anim.SetFloat("Speed", move);
         }
-        
+
+        if (Input.GetKeyDown(KeyCode.Space) && isJumping == false)
+        {
+            isJumping = true;
+            StartCoroutine(StartJump());
+
+        }
+
     }
 
     void Movement()
@@ -31,8 +44,18 @@ public class Walk : MonoBehaviour {
 
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.Translate(-Vector2.right * speedMoove * Time.deltaTime);
-            transform.eulerAngles = new Vector2(0, 0);
+            transform.Translate(Vector2.right * speedMoove * Time.deltaTime);
+            transform.eulerAngles = new Vector2(0, 180);
         }
+    }
+    
+    IEnumerator StartJump()
+    {
+        heroBody.AddForce(new Vector2(0, 350f));
+        heroBody.mass = 1.5f;
+        //heroBody.AddForce(new Vector2(0, -100f));
+        yield return new WaitForSeconds(1);
+        isJumping = false;
+        heroBody.mass = 1f;
     }
 }
